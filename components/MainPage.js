7354dc180348a7ba8e3b65cec3f41ab73063b7ec
AlexandraHessen <xl_energy@mail.ru>
2022-03-09 23:39:26 +0300

    form

---
+++ components/MainPage.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import Form from './Form'
+
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+  }
+}
+
+export default MainPage;
