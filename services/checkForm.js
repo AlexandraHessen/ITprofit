ee62f26120bff971a778a19e9e2c7d720644d2b0
AlexandraHessen <xl_energy@mail.ru>
2022-03-11 23:23:16 +0300

    checkForm

---
+++ services/checkForm.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const checkNameValue = function(name) {
+    if(!name) return 1
+
+    return 0
+
+}
+
+const checkEmailValue = function(email) {
+    if(!email) return 1
+    const reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+    
+    let valid = reg.test(email);
+
+    if( !valid ) return 2
+
+    return 0
+
+}
+
+const checkTelValue = function(tel) {
+    if(!tel) return 1
+    
+    return 0
+
+}
+
+const checkMessageValue = function(message) {
+    if(!message) return 1
+
+    return 0
+
+}
+
+
+
+export { checkNameValue, checkEmailValue, checkTelValue, checkMessageValue };
