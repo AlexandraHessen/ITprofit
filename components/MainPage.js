d929a900bf979681e5e55ed71a56283bc2369da2
AlexandraHessen <xl_energy@mail.ru>
2022-03-11 23:23:50 +0300

    MainPage

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,13 +1,34 @@
-﻿import React from 'react';
-import Form from './Form'
+﻿"use strict";
 
+import React from 'react';
+import Form from './Form'
+import ModalWindow from './ModalWindow'
+
+import './MainPage.css'
 
 class MainPage extends React.Component {
+
+  state={
+    isShowInfo: false
+  }
+
+  showInfo=(value)=>{
+    this.setState( {isShowInfo: value} );
+  }
+
   render() {
     return (
-      <div>
-        <Form />
-      </div>
+    <div className="MainPage">
+          <Form />
+          <input type="button" value="Информация" className="InfoButton" onClick = {() => this.showInfo(true)}></input>
+          {
+            (this.state.isShowInfo) &&
+            <ModalWindow cbShowInfo={this.showInfo}/>
+          }
+          
+    </div>
+
+
     );
   }
 }
